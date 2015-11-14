74d9ca51fa6a1d89015e3f29243f898bbd4e4344
Kevin Avery <kevin@avery.io>
2015-11-14 11:31:31 -0800

    Minor formatting changes.

--- UnsplashDownloader.py
+++ UnsplashDownloader.py
@@ -1,41 +1,43 @@
-import urllib;
-import os,sys,re,logging,chromalog;
+import urllib
+import os, sys, re, logging, chromalog
 from chromalog.mark.helpers.simple import success, error, important
 
-chromalog.basicConfig(level=logging.INFO,format="[%(levelname)s] %(message)s");
-logger = logging.getLogger(__name__);
+chromalog.basicConfig(level=logging.INFO,format="[%(levelname)s] %(message)s")
+logger = logging.getLogger(__name__)
 
-isEnd = False;
-url = "";
-content = "";
-pattern = "<a href=\"/photos/(.*?)\"";
-
+isEnd = False
+url = ""
+content = ""
+pattern = "<a href=\"/photos/(.*?)\""
 directory = os.path.expanduser("~/Pictures/unsplash/")
 
-page  = 1;
-count = 0;
 if not os.path.exists(directory):
-    logger.info("Creating directory");
-    os.makedirs(directory);
+    logger.info("Creating directory")
+    os.makedirs(directory)
 
-while (not isEnd):
-    isEnd = True;
-    url = "http://unsplash.com/?page=" + str(page);
-    content = urllib.urlopen(url).read();
+page = 1
+count = 0
+while not isEnd:
+    isEnd = True
+    url = "http://unsplash.com/?page=" + str(page)
+    content = urllib.urlopen(url).read()
+
     for s in re.findall(pattern,content):
-        pic = "http://unsplash.com/photos/" + s + "/download";
-        filename = directory+ "/" + s + ".jpeg";
-        logger.info("Loading:\t %s",s)
-        if(not os.path.exists(filename)):
-            urllib.urlretrieve(pic,filename);
+        pic = "http://unsplash.com/photos/" + s + "/download"
+        filename = directory + "/" + s + ".jpeg"
 
-            logger.info("Loaded:\t %s (%d)",success(s),count);
+        logger.info("Loading:\t %s", s)
 
-            count+=1;
-            isEnd = False;
+        if not os.path.exists(filename):
+            urllib.urlretrieve(pic, filename)
+
+            logger.info("Loaded:\t %s (%d)", success(s), count)
+
+            count += 1
+            isEnd = False
         else:
+            logger.warning(s + " Exists")
 
-            logger.warning(s + " Exists");
+    page += 1
 
-    page+=1;
-logger.info("%s %d %s",success("Successfully downloaded"),count,success("pictures"));
+logger.info("%s %d %s", success("Successfully downloaded"), count, success("pictures"))
